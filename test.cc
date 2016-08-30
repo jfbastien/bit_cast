3557499863001e32eb5fe77a45ae11d5cc4ff4d2
JF Bastien <jfbastien@apple.com>
2016-08-29 21:47:10 -0700

    Test with a struct

--- test.cc
+++ test.cc
@@ -7,9 +7,9 @@ size_t failures = 0;
 
 #define T(TO, FROM, INPUT, FMT, EXPECT) do {                            \
     char buf[128] = { '\0' };                                           \
-    snprintf(buf, sizeof(buf), FMT, bit_cast<TO>((FROM)(INPUT)));       \
+    snprintf(buf, sizeof(buf), FMT, bit_cast<TO>(FROM INPUT));          \
     std::cout                                                           \
-      << "bit_cast<" #TO ">((" #FROM ")" #INPUT ")\t"                   \
+      << "bit_cast<" #TO ">(" #FROM " " #INPUT ")\t"                    \
       << "-> '" << buf << '\'';                                         \
     if (std::string(buf) != std::string(EXPECT)) {                      \
       ++failures;                                                       \
@@ -18,11 +18,16 @@ size_t failures = 0;
     std::cout << '\n';                                                  \
   } while (false)
 
+struct Float { unsigned mantissa : 23; unsigned exponent : 8; unsigned sign : 1; };
+
 int main() {
-  T(float, uint32_t, 0x00000000,     "%a",     "0x0p+0");
-  T(uint32_t, float,        0.0, "0x%08x", "0x00000000");
-  T(uint32_t, float,       -0.0, "0x%08x", "0x80000000");
-  T(uint32_t, float,        2.0, "0x%08x", "0x40000000");
+  T(   float, uint32_t, (0x00000000),     "%a",     "0x0p+0");
+  T(uint32_t,    float,        (0.0), "0x%08x", "0x00000000");
+  T(uint32_t,    float,       (-0.0), "0x%08x", "0x80000000");
+  T(uint32_t,    float,        (2.0), "0x%08x", "0x40000000");
+  T(   float,    Float,    ({0,0,0}),     "%a",     "0x0p+0");
+  T(   float,    Float,    ({0,0,1}),     "%a",    "-0x0p+0");
+  T(   float,    Float, ({0,0x80,0}),     "%a",     "0x1p+1");
 
   if (failures)
     std::cout << "Failures: " << failures << std::endl;
