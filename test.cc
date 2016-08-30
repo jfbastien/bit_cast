d265f9abede132223d81d9a1c8dc9a55e4e080f3
JF Bastien <jfbastien@apple.com>
2016-08-29 21:32:53 -0700

    Make it nicer.

--- test.cc
+++ test.cc
@@ -1,18 +1,30 @@
 #include "bit_cast.h"
 #include <cstdint>
+#include <cstdio>
 #include <iostream>
 
-#define T(TO, FROM, INPUT) \
-  (std::cout << "bit_cast<" #TO ">((" #FROM ")" << #INPUT << ") -> "	\
-   << bit_cast<TO>((FROM)(INPUT)) << '\n')
+size_t failures = 0;
+
+#define T(TO, FROM, INPUT, FMT, EXPECT) do {                            \
+    char buf[128] = { '\0' };                                           \
+    snprintf(buf, sizeof(buf), FMT, bit_cast<TO>((FROM)(INPUT)));       \
+    std::cout                                                           \
+      << "bit_cast<" #TO ">((" #FROM ")" #INPUT ")\t"                   \
+      << "-> '" << buf << '\'';                                         \
+    if (std::string(buf) != std::string(EXPECT)) {                      \
+      ++failures;                                                       \
+      std::cout << "\texpected: " << (EXPECT);                          \
+    }                                                                   \
+    std::cout << '\n';                                                  \
+  } while (false)
 
 int main() {
-  std::cout << std::hex;
-  T(float, uint32_t, 0x00000000);
-  T(uint32_t, float, 0.0);
-  T(uint32_t, float, -0.0);
-  T(uint32_t, float, 2.0);
+  T(float, uint32_t, 0x00000000,     "%a",     "0x0p+0");
+  T(uint32_t, float,        0.0, "0x%08x", "0x00000000");
+  T(uint32_t, float,       -0.0, "0x%08x", "0x80000000");
+  T(uint32_t, float,        2.0, "0x%08x", "0x40000000");
 
-  std::cout << std::endl;
-  return 0;
+  if (failures)
+    std::cout << "Failures: " << failures << std::endl;
+  return !!failures;
 }
