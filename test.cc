b8a42dc67d2c77c07eeeb6303d6a0cdbb714b7d9
JF Bastien <jfbastien@apple.com>
2016-09-04 16:59:05 -0700

    Complex

--- test.cc
+++ test.cc
@@ -1,5 +1,6 @@
 #include "bit_cast.h"
 #include <cmath>
+#include <complex>
 #include <cstdint>
 #include <cstdio>
 #include <iostream>
@@ -49,39 +50,42 @@ typedef __attribute__((vector_size(4))) uint8_t V4x8;
 // Array To doesn't make sense?
 
 int main() {
-  //               TO              FROM         INPUT   ACCESS       FMT        EXPECT
-  T(            float,         uint32_t, (0x00000000),        ,     "%a",     "0x0p+0");
-  T(         uint32_t,            float,        (0.0),        , "0x%08x", "0x00000000");
-  T(         uint32_t,            float,       (-0.0),        , "0x%08x", "0x80000000");
-  T(         uint32_t,            float,        (2.0),        , "0x%08x", "0x40000000");
-  T(   const uint32_t,    (const float),        (2.0),        , "0x%08x", "0x40000000");
-  T(         uint32_t, (volatile float),        (2.0),        , "0x%08x", "0x40000000");
-  T(            float,            Float,    ({0,0,0}),        ,     "%a",     "0x0p+0");
-  T(            float,            Float,    ({0,0,1}),        ,     "%a",    "-0x0p+0");
-  T(            float,            Float, ({0,0x80,0}),        ,     "%a",     "0x1p+1");
-  T(           Padded,            float,        (2.f),      .c, "0x%02x",       "0x00");
-  T(           Padded,            float,        (2.f),      .s, "0x%04x",     "0x4000");
-  T(           NoCtor,            float,        (2.f),      .u, "0x%08x", "0x40000000");
-  T(          Private,            float,        (2.f),  .get(), "0x%08x", "0x40000000");
-  T(            Const,            float,        (2.f),      .u, "0x%08x", "0x40000000");
-  T(         Volatile,            float,        (2.f),      .u, "0x%08x", "0x40000000");
-  T(    ConstVolatile,            float,        (2.f),      .u, "0x%08x", "0x40000000");
-  T(DefaultMemberInit,            float,        (2.f),      .u, "0x%08x", "0x40000000");
-  T(            Union,            float,        (2.f),      .u, "0x%08x", "0x40000000");
-  T(      UnionNoCtor,            float,        (2.f),    .s.u, "0x%08x", "0x40000000");
-  T(      StructArray,            float,        (2.f), .arr[0], "0x%02x",       "0x00");
-  T(      StructArray,            float,        (2.f), .arr[1], "0x%02x",       "0x00");
-  T(      StructArray,            float,        (2.f), .arr[2], "0x%02x",       "0x00");
-  T(      StructArray,            float,        (2.f), .arr[3], "0x%02x",       "0x40");
-  T(        ZeroWidth,            float,        (2.f),      .u, "0x%08x", "0x40000000");
-  T(       ZeroWidthC,            float,        (2.f),      .u, "0x%08x", "0x40000000");
-  T(          Recurse,            float,        (2.f),      .u, "0x%08x", "0x40000000");
-  T(      RecurseInit,            float,        (2.f),      .u, "0x%08x", "0x40000000");
-  T(   RecurseAggInit,            float,        (2.f),      .u, "0x%08x", "0x40000000");
-  T(             V4x8,         uint32_t, (0xdeadbeef),     [0], "0x%02x",       "0xef");
-  T(             V4x8,         uint32_t, (0xdeadbeef),     [1], "0x%02x",       "0xbe");
-  T(             V4x8,         uint32_t, (0xdeadbeef),     [2], "0x%02x",       "0xad");
-  T(             V4x8,         uint32_t, (0xdeadbeef),     [3], "0x%02x",       "0xde");
+  //                    TO              FROM         INPUT   ACCESS       FMT        EXPECT
+  T(                 float,         uint32_t, (0x00000000),        ,     "%a",     "0x0p+0");
+  T(              uint32_t,            float,        (0.0),        , "0x%08x", "0x00000000");
+  T(              uint32_t,            float,       (-0.0),        , "0x%08x", "0x80000000");
+  T(              uint32_t,            float,        (2.0),        , "0x%08x", "0x40000000");
+  T(        const uint32_t,    (const float),        (2.0),        , "0x%08x", "0x40000000");
+  T(              uint32_t, (volatile float),        (2.0),        , "0x%08x", "0x40000000");
+  T(                 float,            Float,    ({0,0,0}),        ,     "%a",     "0x0p+0");
+  T(                 float,            Float,    ({0,0,1}),        ,     "%a",    "-0x0p+0");
+  T(                 float,            Float, ({0,0x80,0}),        ,     "%a",     "0x1p+1");
+  T(                Padded,            float,        (2.f),      .c, "0x%02x",       "0x00");
+  T(                Padded,            float,        (2.f),      .s, "0x%04x",     "0x4000");
+  T(                NoCtor,            float,        (2.f),      .u, "0x%08x", "0x40000000");
+  T(               Private,            float,        (2.f),  .get(), "0x%08x", "0x40000000");
+  T(                 Const,            float,        (2.f),      .u, "0x%08x", "0x40000000");
+  T(              Volatile,            float,        (2.f),      .u, "0x%08x", "0x40000000");
+  T(         ConstVolatile,            float,        (2.f),      .u, "0x%08x", "0x40000000");
+  T(     DefaultMemberInit,            float,        (2.f),      .u, "0x%08x", "0x40000000");
+  T(                 Union,            float,        (2.f),      .u, "0x%08x", "0x40000000");
+  T(           UnionNoCtor,            float,        (2.f),    .s.u, "0x%08x", "0x40000000");
+  T(           StructArray,            float,        (2.f), .arr[0], "0x%02x",       "0x00");
+  T(           StructArray,            float,        (2.f), .arr[1], "0x%02x",       "0x00");
+  T(           StructArray,            float,        (2.f), .arr[2], "0x%02x",       "0x00");
+  T(           StructArray,            float,        (2.f), .arr[3], "0x%02x",       "0x40");
+  T(             ZeroWidth,            float,        (2.f),      .u, "0x%08x", "0x40000000");
+  T(            ZeroWidthC,            float,        (2.f),      .u, "0x%08x", "0x40000000");
+  T(               Recurse,            float,        (2.f),      .u, "0x%08x", "0x40000000");
+  T(           RecurseInit,            float,        (2.f),      .u, "0x%08x", "0x40000000");
+  T(        RecurseAggInit,            float,        (2.f),      .u, "0x%08x", "0x40000000");
+  T(                  V4x8,         uint32_t, (0xdeadbeef),     [0], "0x%02x",       "0xef");
+  T(                  V4x8,         uint32_t, (0xdeadbeef),     [1], "0x%02x",       "0xbe");
+  T(                  V4x8,         uint32_t, (0xdeadbeef),     [2], "0x%02x",       "0xad");
+  T(                  V4x8,         uint32_t, (0xdeadbeef),     [3], "0x%02x",       "0xde");
+  T(std::complex<uint16_t>,         uint32_t, (0xdeadbeef), .real(), "0x%04x",     "0xbeef");
+  T(std::complex<uint16_t>,         uint32_t, (0xdeadbeef), .imag(), "0x%04x",     "0xdead");
+  //T(        _Complex short,         uint32_t, (0xdeadbeef),        , "0x%04x",     "0xbeef");
 
   std::cout << "&main as uintptr_t:\t" << std::hex << "0x" << bit_cast<uintptr_t>(&main) << '\n';
   std::cout << "&main as void*:\t" << std::hex << bit_cast<void*>(&main) << '\n';
